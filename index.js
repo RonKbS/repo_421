b0fc8d85c07293205b045e080b23fce881e4aab1
RonKbS <stephen.bamwite@gmail.com>
2022-04-09 19:02:27 +0300

    update chart following input of number

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 
 
-let givenVal = 500
-let vals421 = []
+let givenVal = 7
+let vals421, timeOutIds = []
 
-while (vals421[vals421.length - 1] !== 1) {
-  if (!vals421[vals421.length - 1]) {
-    vals421.push(givenVal)
+let fillVals421 = val_ => {
+  vals421 = []
+  while (vals421[vals421.length - 1] !== 1) {
+    if (!vals421[vals421.length - 1]) {
+      vals421.push(givenVal)
+    }
+    let getNextNo = vals421[vals421.length - 1]
+    getNextNo = getNextNo % 2 === 0 ? getNextNo / 2 : (getNextNo * 3) + 1;
+    vals421.push(getNextNo)
   }
-  let getNextNo = vals421[vals421.length - 1]
-  getNextNo = getNextNo % 2 === 0 ? getNextNo / 2 : (getNextNo * 3) + 1;
-  vals421.push(getNextNo)
 }
+fillVals421();
 
 
 // specify the x and y axies
@@ -22,7 +26,7 @@ let createLineChart = () => {
       labels: vals421,
       datasets: [{
         label: '55',
-        data: vals421.slice(0, 1),
+        data: vals421,
         borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 1
       }]
@@ -32,6 +36,15 @@ let createLineChart = () => {
         y: {
           beginAtZero: true
         }
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: `${givenVal} - ${vals421.length} numbers to get back to 1`
+        },
+        legend: {
+          display: false
+        }
       }
     }
   });
@@ -40,10 +53,31 @@ let createLineChart = () => {
 
 let chartInstance = createLineChart()
 
+$("#mesmerizeM").on("click", e => {
+  givenVal = parseInt($("#inputVal").val())
 
-vals421.slice(1, vals421.length).forEach((val_, index) => setTimeout(
-    () => {
-      chartInstance.data.datasets[0].data.push(val_); chartInstance.update()
-    }, index * 500
+  if (typeof(givenVal) === "number" && givenVal > 0) {
+
+
+  fillVals421();
+
+  let timeTaken = 5;
+  if (vals421.length > 30) {
+    timeTaken = parseInt(vals421.length/50)
+  }
+  chartInstance.options.plugins.title.text = `${givenVal} - ${vals421.length} numbers to get back to 1`
+  
+  
+  vals421.slice(1, vals421.length).forEach((val_, index) => setTimeout(
+      () => {
+        chartInstance.data.datasets[0].data.shift();
+        chartInstance.data.datasets[0].data.push(val_);
+        chartInstance.update()
+      }, index * timeTaken * 100
+    )
   )
-)
+  } else {
+    alert(`${givenVal} is not a positive number`)
+  }
+})
+
